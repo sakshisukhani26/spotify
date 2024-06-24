a3a5dddef13f037695af83d8cdf8a8c957858058
hp <sakshisukhani26@gmail.com>
2024-06-24 13:35:22 +0530

    intial commit

---
+++ lib/firstscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Firstscreen extends StatefulWidget {
+  const Firstscreen({super.key});
+
+  @override
+  State<Firstscreen> createState() => _FirstscreenState();
+}
+
+class _FirstscreenState extends State<Firstscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/splash.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'firstscreen.dart';
+
+class SplashScreen2 extends StatefulWidget{
+  const SplashScreen2({super.key});
+  @override
+  State<SplashScreen2> createState()=>_SplashScreenState();
+
+}
+
+class _SplashScreenState extends State<SplashScreen2>{
+  // @override
+  // void initState() {
+  //   Timer (Duration(seconds: 3),() {
+  //     Navigator.push(context, MaterialPageRoute(builder: (context)=>Firstscreen()));
+  //   });
+  //   super.initState();
+  // }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircleAvatar(
+          radius: 100,
+          backgroundImage: NetworkImage('https://play-lh.googleusercontent.com/7ynvVIRdhJNAngCg_GI7i8TtH8BqkJYmffeUHsG-mJOdzt1XLvGmbsKuc5Q1SInBjDKN=w240-h480-rw',),
+        ),
+      ),
+      backgroundColor: Colors.black38,
+    );
+  }
+}
